a0efe53ac1e87e0adcb2ab33fa7955f42183866d
YourBoyMaleek96 <malikfreeman1996@gmail.com>
2023-10-31 16:54:53 -0400

    added login and online status

--- GUI/MainGUI.py
+++ GUI/MainGUI.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from tkinter import * 
 import tkinter
 
 app = tkinter.Tk() #main window for gui 
@@ -7,11 +8,21 @@ app.title("Video Game Profile")
 ctk.set_appearance_mode("Dark") #Set theme to dark mode 
 ctk.set_default_color_theme("blue")
 frame = ctk.CTkFrame(master = app)
+frame._set_appearance_mode("Dark")
 frame.pack(pady=20,padx=40,fill = 'both' ,expand = True)
 label = ctk.CTkLabel(master=frame, text = 'Login Page')
 label.pack(pady = 12, padx=10)
-username_textbox = ctk.CTkEntry(master =frame, placeholder_text = "Enter Username")
-username_textbox.pack(pady = 12, padx = 10)
+UsernameTextbox = ctk.CTkEntry(master =frame, placeholder_text = "Enter Username")
+UsernameTextbox.pack(pady = 12, padx = 10)
+
+#Drop down for online status 
+OnlineDrop = ctk.CTkComboBox(master=frame, values = ["Online", "Offline", "Busy"])
+OnlineDrop.pack(pady = 12, padx = 10)
+
+#Login Button
+LoginButton = ctk.CTkButton(master=frame, text="Login")
+LoginButton.pack(pady=12, padx=10)
+
 
 
 app.mainloop() #end gui window 
