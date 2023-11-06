71fcea1265d3727335a0ff997fc6772737a28c2d
Westley Bridges <westley.t.bridges@gmail.com>
2023-11-06 10:38:50 -0500

    Added logic to read games file

--- GameList.py
+++ GameList.py
@@ -6,8 +6,22 @@ class Game:
         self.num_achievements = num_achievements
 
     def game_info(self):
-        print(f"Game: {self.title}")
-        print(f"Hours: {self.hours}")
-        print(f"Achievements: {self.achievements}")
+        print(f"Game: {self.game_title}")
+        print(f"Hours: {self.hours_played}")
+        print(f"Achievements: {self.num_achievements}")
 
 game_list = []
+
+with open('GameList/games.txt', 'r') as file:
+    for line in file:
+        game_data = line.strip().split(',')
+        if len(game_data) == 3:
+            title = game_data[0].strip()
+            hours = int(game_data[1].strip())
+            achievements = int(game_data[2].strip())
+
+            game = Game(title, hours, achievements)
+            game_list.append(game)
+
+for game in game_list:
+    game.game_info()
