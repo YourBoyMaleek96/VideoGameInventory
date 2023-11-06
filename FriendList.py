548d82798625c0613dbeba8179ec4fabcc2433fe
Westley Bridges <westley.t.bridges@gmail.com>
2023-11-06 10:53:49 -0500

    Added logic to parse friend list file

--- FriendList.py
+++ FriendList.py
@@ -1,12 +1,26 @@
 
 class Friend:
-    def __init__(self, user_name, real_name, player_score):
+    def __init__(self, user_name, real_name, last_online):
         self.user_name = user_name
         self.real_name = real_name
-        self.player_score = player_score
+        self.last_online = last_online
     def friend_info(self):
         print(f"Username: {self.user_name}")
         print(f"Name: {self.real_name}")
-        print(f"Player Score: {self.player_score}")
+        print(f"Last Online (hours): {self.last_online}")
 
 friend_list = []
+
+with open('FriendList/friends.txt', 'r') as file:
+    for line in file:
+        friend_data = line.strip().split(',')
+        if len(friend_data) == 3:
+            userName = friend_data[0].strip()
+            realName = friend_data[1].strip()
+            lastOnline = int(friend_data[2].strip())
+
+            friend = Friend(userName, realName, lastOnline)
+            friend_list.append(friend)
+
+for friend in friend_list:
+    friend.friend_info()
