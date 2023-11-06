7ee516171a67cb9e1d85d22dced7d4f6df0c2148
Westley Bridges <westley.t.bridges@gmail.com>
2023-11-06 14:28:05 -0500

    Created main file and put list initialization in functions

---
+++ main.py
@@ -0,0 +1,8 @@
+import GameList
+import FriendList
+
+#Create and load game array
+user_game_list = GameList.initialize_game_list('GameList/games.txt')
+
+#Create and load friend array
+user_friend_list = FriendList.initialize_friend_list('FriendList/friends.txt')
