e0e9c2736ea5a6f997ef7881cc34bb4754cdd003
Westley Bridges <westley.t.bridges@gmail.com>
2023-11-11 15:00:32 -0500

    Revert "Testing out Friend List and Game List GUI on Main"
    This reverts commit d8b7279e661cc6498008f3f98bd044a6250346e4.

--- GameList.py
+++ GameList.py
@@ -1,49 +1,49 @@
-#Creates the class "Game" 
-import tkinter as tk 
-from tkinter import ttk 
-class Game: 
-    #Initializes members of game object 
-    def __init__(self, game_title, hours_played, num_achievements): 
-        self.game_title = game_title 
-        self.hours_played = hours_played 
-        self.num_achievements = num_achievements 
- 
-    def __str__(self): 
-        return (f"Game Title: {self.game_title}\n Hours Played: {self.hours_played}\n Number of Achievements: {self.num_achievements}") 
- 
-#method that fills the global games list array with the data from games.txt 
-def initialize_game_list(file_path): 
-    game_list = [] 
-    with open(file_path, 'r') as file: 
-        for line in file: 
-            game_data = line.strip().split(',') 
-            if len(game_data) == 3: 
-                title = game_data[0].strip() 
-                hours = int(game_data[1].strip()) 
-                achievements = int(game_data[2].strip()) 
- 
-                game = Game(title, hours, achievements) 
-                game_list.append(game) 
-     
-    return game_list 
- 
-# Create a GUI for the Window List 
-app = tk.Tk() 
-app.title("Game List") 
-# Created a Button in order to Show the Game List on GameList/games.txt 
-display_button = tk.Button(app, text = "Update Game List", command = display_game_list) 
- 
-display_button.pack() 
- 
-# Creating a scroll bar to look through the game list 
-scrollbar = tk.Scrollbar(app) 
-scrollbar.pack(side=tk.RIGHT, fill = tk.Y) 
- 
-# Create a text box to display the game list with a scrollbar 
-text_box = tk.Text(app, width=60, height=20, yscrollcommand = scrollbar.set) 
-text_box.pack() 
- 
-scrollbar.config(command=text_box.yview) 
-# Run the GUI (Temporary) 
- 
+#Creates the class "Game"
+import tkinter as tk
+from tkinter import ttk
+class Game:
+    #Initializes members of game object
+    def __init__(self, game_title, hours_played, num_achievements):
+        self.game_title = game_title
+        self.hours_played = hours_played
+        self.num_achievements = num_achievements
+
+    def __str__(self):
+        return (f"Game Title: {self.game_title}\n Hours Played: {self.hours_played}\n Number of Achievements: {self.num_achievements}")
+
+#method that fills the global games list array with the data from games.txt
+def initialize_game_list(file_path):
+    game_list = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            game_data = line.strip().split(',')
+            if len(game_data) == 3:
+                title = game_data[0].strip()
+                hours = int(game_data[1].strip())
+                achievements = int(game_data[2].strip())
+
+                game = Game(title, hours, achievements)
+                game_list.append(game)
+    
+    return game_list
+
+# Create a GUI for the Window List
+app = tk.Tk()
+app.title("Game List")
+# Created a Button in order to Show the Game List on GameList/games.txt
+display_button = tk.Button(app, text = "Update Game List", command = display_game_list)
+
+display_button.pack()
+
+# Creating a scroll bar to look through the game list
+scrollbar = tk.Scrollbar(app)
+scrollbar.pack(side=tk.RIGHT, fill = tk.Y)
+
+# Create a text box to display the game list with a scrollbar
+text_box = tk.Text(app, width=60, height=20, yscrollcommand = scrollbar.set)
+text_box.pack()
+
+scrollbar.config(command=text_box.yview)
+# Run the GUI (Temporary)
+
 app.mainloop()
--- mainCheng.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-import GameListCheng
-import FriendListCheng
-
-# Create and load game array
-user_game_list = GameListCheng.initialize_game_list('GameList/games.txt')
-
-# Create and load friend array
-user_friend_list = FriendListCheng.initialize_friend_list('FriendList/friends.txt')
