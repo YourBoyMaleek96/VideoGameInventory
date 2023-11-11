f2a3c3100101cae2bacc97602cac1d8332957114
Westley Bridges <westley.t.bridges@gmail.com>
2023-11-11 15:47:37 -0500

    Cleaned list files and created test file

--- GameList.py
+++ GameList.py
@@ -1,6 +1,4 @@
 #Creates the class "Game"
-import tkinter as tk
-from tkinter import ttk
 class Game:
     #Initializes members of game object
     def __init__(self, game_title, hours_played, num_achievements):
@@ -8,6 +6,7 @@ class Game:
         self.hours_played = hours_played
         self.num_achievements = num_achievements
 
+    #Prints members of game object
     def __str__(self):
         return (f"Game Title: {self.game_title}\n Hours Played: {self.hours_played}\n Number of Achievements: {self.num_achievements}")
 
@@ -26,24 +25,3 @@ def initialize_game_list(file_path):
                 game_list.append(game)
     
     return game_list
-
-# Create a GUI for the Window List
-app = tk.Tk()
-app.title("Game List")
-# Created a Button in order to Show the Game List on GameList/games.txt
-display_button = tk.Button(app, text = "Update Game List", command = display_game_list)
-
-display_button.pack()
-
-# Creating a scroll bar to look through the game list
-scrollbar = tk.Scrollbar(app)
-scrollbar.pack(side=tk.RIGHT, fill = tk.Y)
-
-# Create a text box to display the game list with a scrollbar
-text_box = tk.Text(app, width=60, height=20, yscrollcommand = scrollbar.set)
-text_box.pack()
-
-scrollbar.config(command=text_box.yview)
-# Run the GUI (Temporary)
-
-app.mainloop()
--- main.py
+++ main.py
@@ -6,3 +6,9 @@ user_game_list = GameList.initialize_game_list('GameList/games.txt')
 
 #Create and load friend array
 user_friend_list = FriendList.initialize_friend_list('FriendList/friends.txt')
+
+#Debug print statements
+for game in user_game_list:
+    print(game)
+for friend in user_friend_list:
+    print(friend)
