011254848b0cc680cba41de6b96fa154a3dd6b6e
Westley Bridges <westley.t.bridges@gmail.com>
2023-11-13 10:30:11 -0500

    Add and Remove Game Functionality and Error Handling

--- GameList.py
+++ GameList.py
@@ -13,15 +13,48 @@ class Game:
 #method that fills the global games list array with the data from games.txt
 def initialize_game_list(file_path):
     game_list = []
-    with open(file_path, 'r') as file:
-        for line in file:
-            game_data = line.strip().split(',')
-            if len(game_data) == 3:
-                title = game_data[0].strip()
-                hours = int(game_data[1].strip())
-                achievements = int(game_data[2].strip())
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                game_data = line.strip().split(',')
+                if len(game_data) == 3:
+                    title = game_data[0].strip()
+                    hours = int(game_data[1].strip())
+                    achievements = int(game_data[2].strip())
 
-                game = Game(title, hours, achievements)
-                game_list.append(game)
-    
+                    game = Game(title, hours, achievements)
+                    game_list.append(game)
+    except FileNotFoundError:
+        print(f"Error: The file {file_path} was not found.")
+    except ValueError:
+        print("Error: Invalid data format in the file.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
     return game_list
+
+#Add Game Function
+def add_game(game_list, title, hours, achievements):
+    try:
+        hours = int(hours)
+        achievements = int(achievements)
+        new_game = Game(title, hours, achievements)
+        game_list.append(new_game)
+    except ValueError:
+        print("Invalid data: 'hours played' and 'number of achievements' must be integers.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+#Remove Game Function
+def remove_game(game_list, title):
+    try:
+        for i, game in enumerate(game_list):
+            if game.game_title == title:
+                del game_list[i]
+                print(f"Removed game: {title}")
+                return
+        
+        raise ValueError(f"No game found with title: {title}")
+    except ValueError as e:
+        print(e)
+
