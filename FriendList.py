f2412aa60dbc0da137a00edae469ab7bdcece599
Westley Bridges <westley.t.bridges@gmail.com>
2023-11-13 09:44:35 -0500

    Add and Remove Friend Functionality and Error Handling

--- FriendList.py
+++ FriendList.py
@@ -13,15 +13,41 @@ class Friend:
 #method that fills the global friend list array with the data from friends.txt
 def initialize_friend_list(file_path):
     friend_list = []
-    with open(file_path, 'r') as file:
-        for line in file:
-            friend_data = line.strip().split(',')
-            if len(friend_data) == 3:
-                userName = friend_data[0].strip()
-                realName = friend_data[1].strip()
-                lastOnline = int(friend_data[2].strip())
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                friend_data = line.strip().split(',')
+                if len(friend_data) == 3:
+                    userName = friend_data[0].strip()
+                    realName = friend_data[1].strip()
+                    lastOnline = int(friend_data[2].strip())
 
-                friend = Friend(userName, realName, lastOnline)
-                friend_list.append(friend)
+                    friend = Friend(userName, realName, lastOnline)
+                    friend_list.append(friend)
+    except FileNotFoundError:
+        print(f"Error: The file {file_path} was not found.")
+    except ValueError:
+        print("Error: Invalid data format in the file.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
     return friend_list
+
+
+# Add Friend Function
+def add_friend(friend_list, user_name, real_name, last_online):
+    new_friend = Friend(user_name, real_name, last_online)
+    friend_list.append(new_friend)
+
+# Remove Friend Function
+def remove_friend(friend_list, user_name):
+    try:
+        for i, friend in enumerate(friend_list):
+            if friend.user_name == user_name:
+                del friend_list[i]
+                print(f"Removed friend: {user_name}")
+                return
+            
+        raise ValueError(f"No friend found with username: {user_name}")
+    except ValueError as e:
+        print(e)
